73de5f2fd7bc1b8d508882bc92ea4dbf65f17b4b
kagemusha <mmadrid99@gmail.com>
2014-05-12 09:56:25 +0800

    seed a user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+#user = CreateAdminService.new.call
+#puts 'CREATED ADMIN USER: ' << user.email
 # Environment variables (ENV['...']) can be set in the file .env file.
+
+user = User.create :name => "madmad", :email => "t@t.com", :password => "tester", :password_confirmation =>"tester"
+

96599cf7e9fc437015cc5c1b61e29d6ceade4f5c
kagemusha <mmadrid99@gmail.com>
2014-05-13 13:52:39 +0800

    set up current_user, correct forgery protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
   include Pundit
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
+
+  def current_user
+    @current_user ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 
   def user_not_authorized
     flash[:alert] = "Access denied."

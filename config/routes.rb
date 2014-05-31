93a871340a8bab235e355fb8203ba8ac5a8853ee
kagemusha <mmadrid99@gmail.com>
2014-05-31 18:31:45 -0400

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 EmblogServer::Application.routes.draw do
+  use_doorkeeper
+
+  get 'users/current_user', to: 'users#current'
+  devise_for :users, :controllers => {:registrations => "registrations"}
+
+  get 'posts/?type=contributor', to: 'posts#contributed'
   resources :posts
 
-  use_doorkeeper
   root :to => "home#index"
-  get 'users/current_user', to: 'users#current'
-  get 'posts/?type=contributor', to: 'posts#contributed'
-  devise_for :users, :controllers => {:registrations => "registrations"}
 end

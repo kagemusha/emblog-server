69710a033058e8f0c7cde6b20915bade20e3d5bf
kagemusha <mmadrid99@gmail.com>
2014-06-17 19:28:36 -0400

    fix roles (though not really using)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
   rolify
   has_many :posts, foreign_key: "author_id"
+  has_many :comments
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  enum role: [:user, :vip, :admin]
+  enum role: [:user, :contributor, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role

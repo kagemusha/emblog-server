55713889ad70b51784a59ffb92c36ddae2f63986
kagemusha <mmadrid99@gmail.com>
2014-06-17 22:48:54 -0400

    add author to post and author serializer

---
+++ app/serializers/author_serializer.rb
@@ -0,0 +1,4 @@
+class AuthorSerializer < ActiveModel::Serializer
+  attributes :id, :email, :name
+
+end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
+  embed :ids, include: true
   attributes :id, :title, :markdown, :html, :status, :short, :published_at, :created_at, :updated_at
+  has_one  :author, serializer: AuthorSerializer
 end
